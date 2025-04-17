36e5c0c8f8067f2ff99669e9a8f11cb9587c0814
yuye-aws <yuyezhu@amazon.com>
2025-04-17 07:56:33 +0000

    remove the ingestion script
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- ingest.sh
+++
@@ -1,6 +0,0 @@
-python3 examples/dense_exact_search.py;
-python3 examples/dense_hnsw_search.py;
-python3 examples/ hybrid_search.py;
-python3 examples/lexical_search.py;
-python3 examples/conversational_search.py;
-python3 examples/sparse_search.py;
